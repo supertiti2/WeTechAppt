e70e54b5f4c150997a1053d796938b6f8b79ba92
supertti <supertti@foxmail.com>
2022-03-28 12:48:37 +0800

    CR

---
+++ miniprogram/projects/A00/skin/skin.js
@@ -0,0 +1,43 @@
+module.exports = {
+	PID: 'A00', // 科技馆
+
+	NAV_COLOR: '#ffffff',
+	NAV_BG: '#0D97E1',
+
+	MEET_NAME: '预约',
+
+	MENU_ITEM: ['首页', '预约日历', '我的'], // 第1,4,5菜单
+ 
+	NEWS_CATE: '1=本馆动态|leftbig,2=参观推荐|leftbig,3=青少年科技,4=服务指南',
+	MEET_TYPE: '1=入馆预约|leftbig2,2=活动预约|leftbig3',
+
+	DEFAULT_FORMS: [{
+			type: 'line',
+			title: '姓名',
+			desc: '请填写您的姓名',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		},
+		{
+			type: 'line',
+			title: '手机',
+			desc: '请填写您的手机号码',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		}
+	]
+}
